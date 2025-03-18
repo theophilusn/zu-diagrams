da1d4a4671d5706b835596d5f361e4162c4aa396
theophilusn <theophilus.nenhanga@zu.com>
2025-03-18 16:06:44 -0600

    feat: added docker compose

--- uploader/app.py
+++ uploader/app.py
@@ -1,12 +1,16 @@
 import os
-import datetime 
 from flask import Flask, request, redirect, render_template
 
 app = Flask(__name__)
 
+def is_allowed_file(filename: str)->bool:
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'dsl'}
+
 @app.route('/upload', methods=['POST'])
 def upload():
     file = request.files['file']
+    if not (file and is_allowed_file(file.filename)):
+        return 'Invalid file', 400
     file.save(os.path.join('uploads', file.filename))
     return redirect('/')
 
@@ -15,4 +19,4 @@ def index():
     return render_template('base.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", debug=True)
